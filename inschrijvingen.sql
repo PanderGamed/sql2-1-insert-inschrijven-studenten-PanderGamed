9b98d891a83fdabf02d5b3049fafe3152caf5a86
PanderGamed <48210381+PanderGamed@users.noreply.github.com>
2019-05-13 19:17:16 +0200

    Opdracht 2
    Gemaakt

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,3 +1,6 @@
+
+
+OPDRACHT 1
 USE `lo8e_sql2`;
 
 SELECT * FROM docenten;
@@ -6,3 +9,14 @@ INSERT INTO klassen
 	('lo9e-amo1','bkr02', 2019, 'amo'), 
 	('lo9e-amo2','vlt03', 2019, 'amo'), 
     ('lo9e-amo3','rs001', 2019, 'amo');
+	
+	
+	
+	OPDRACHT 2
+	
+	INSERT INTO studenten    (ov_nummer, achternaam, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, actief, inschrijvings_datum)
+VALUES 
+("90001", "Pander", "Sander","2001-11-08","2644WE", "Den-haag","80.1","79.1","1","2019-05-13"),
+("90002", "Tillekens", "Tim","2000-05-07","2342WD", "Den-haag","75.1","75.1","1","2019-05-13"),
+("90003", "vermeij", "Wessel","2002-05-05","2344WE", "Leiden","85.1","77.1","1","2019-05-13"),
+("90004", "Hardoar", "Marciano","2002-07-07","2544WD", "Den-haag","82.1","78.1","1","2019-05-13");
